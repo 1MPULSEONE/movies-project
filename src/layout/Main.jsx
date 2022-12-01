41a43913788e830d8ef51ac24954a016ea7e31ba
1MPULSEONE <parkhomenko2049@gmail.com>
2022-12-01 16:34:45 +0600

    Movies commit. Add Movie.jsx Add Movies.jsx Updated Main.jsx and some other files

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,27 @@
-function Main() {
-    return (
-        <main className="main-container content">
-            Hello from Movies App
-        </main>
-    )
+import React from 'react';
+import { Movies } from '../components/Movies';
+
+class Main extends React.Component {
+    state = {
+        movies: [],
+    }
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=fa56eac5&s=blade runner')
+            .then(response => response.json())
+            .then(data => this.setState({ movies: data.Search }))
+    }
+    render() {
+        const { movies } = this.state;
+        return (
+            <main className="main-container content" >
+                {
+                    movies.length
+                        ? (<Movies movies={movies} />) : <h3>Loading...</h3>
+                }
+            </main>
+        )
+    }
 }
 
-export { Main }
+export { Main };
 
